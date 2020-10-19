eddd90363ceedded610f0f35d94e5b7a9d1cc3ee
Andrea <andrea.vld.zrc@gmail.com>
2020-10-19 10:58:41 -0400

    'heroku'

--- client/src/Components/Work.js
+++ client/src/Components/Work.js
@@ -10,7 +10,7 @@ function Work(props) {
 
             <div id='workMini'>
                 <ul>
-                    
+                    <li>Piedmont Airlines - Gate Service Agent  02/2020 - current</li> 
                     <li>Cheeseman Insurance Group - Commercial Lines Assistant   07/2019 - 09/2019</li>
                     <li>NFP -   Account Manager - 03/2019  -  06/2019 </li>
                     <li>DealerPolicy - Winooski Insurance - Personal Lines Client Advisor - 01/2018 - 09/2018</li>
--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.static(path.resolve('./client/build')))
+app.get('*',(request,response) =>{
+response.sendFile(pathresolve('./client/build/index.html'))
+})
+
+app.listen(port, () =>
+console.log('Listening on port: ${port}'))
+
